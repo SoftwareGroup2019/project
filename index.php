c5b27816d7623973cda51fe79d2b010290c35a1a
softwaregroup2019 <mohamadit69@gmail.com>
2019-11-09 18:39:23 +0300

    saqataka

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 
 echo "Hello this is user page";
 echo "Test";
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 0e356192bddf2cb90482b0c90c870323f3cdc28e
  echo "test2";
+
+echo "hello this file saqataka";
+
  ?>
