7ec9d24008aafc4665e3115ac2b2aadd0cdb78b0
softwaregroup2019 <mohamadit69@gmail.com>
2019-11-11 13:08:55 -0800

    done #hawar

--- Admin/index.php
+++ Admin/index.php
@@ -1,5 +1,38 @@
 <?php include 'include/template/header.php';?>
 
+<div class="login-div">
+  <div class="row ">
+
+    <h5> sign in <h5>
+    <h6>admin you are red stepler Account</h6>
+  </div>
+<div class="row center-align">
+  <div class="input-field col s12">
+<input id ="email-input" type="email" class="validate">
+<label for ="email-input"> email </label>
+</div>
+</div>
+
+<div class="row">
+  <div class="input-field col s12">
+<input id ="password-input" type="password" class="validate">
+<label for ="password-input"> password </label>
+<div><a href="#"><b>forgot password ?</b></a>
+</div>
+</div>
+<div class="row">
+<div class="col s12"> not your computer ? use a private windos to sing in <a href="#"><b>learn more</b>
+</div>
+<div class="row">
+<div class="row">
+  <div class="col s6"><a href ="#">creat Account</a>
+</div>
+<div class="col s6 right.laign"><a class="waves-effect waves-light btn" >login</a>
+</div>
+</div>
+</div>
+
+
 
 
 
