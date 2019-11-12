d130cde941c41d3c224b4b8adceaaf25abfa7b09
softwaregroup2019 <mohamadit69@gmail.com>
2019-11-12 08:37:44 +0300

    data base connect

---
+++ Admin/conect.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$dsn='mysql:host=localhost;dbname=shop';
+$user= 'root';
+$pass= '';
+$option= array (
+PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8';
+
+
+
+);
+try {
+  $con = new PDO ($dsn , $pass, $option);
+  $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'you are connected Welcome to Database ';
+
+}
+catch(PDOException $e){
+  echo 'Failed to connect' . $e->getMessage();
+}
+
+
+
+
+
+
+
+?>
