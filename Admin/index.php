5f43ae7826c2d02e60e920f7e4ebb27dcbee77b7
SoftwareGroup2019 <mohamadit69@gmail.com>
2019-11-12 22:59:26 +0300

    test with ayman

--- Admin/index.php
+++ Admin/index.php
@@ -1,16 +1,18 @@
 <?php include 'include/template/header.php';?>
-<div class="col s12 m2">
-  <p class="z-depth 2"></p>
-<div class="login-div">
-  <div class="row ">
 
-    <h5> Sign in <h5>
-    <h6>Admin you are red stepler Account</h6>
+
+<div class="z-depth-2 login-div">
+  <div class="row ">
+<div class="col s12 ">
+  <h5 > Sign in <h5>
+  <h6 >Welcome Admin</h6>
+</div>
+
   </div>
-<div class="row center-align">
+<div class="row">
   <div class="input-field col s12">
-<input id ="Email-input" type="Email" class="validate">
-<label for ="Email-input"> Email </label>
+<input id ="text-input" type="text" class="validate">
+<label for ="text-input"> UserName </label>
 </div>
 </div>
 
@@ -21,21 +23,16 @@
 <div><a href="#"><b>Forgot Pssword ?</b></a>
 </div>
 </div>
-<div class="row">
-<div class="col s12">   Admin private windos to singin     <a href="#"><b>Learn more</b>
 </div>
 
 <div class="row">
-  <div class="col s6"><a href ="#"> Creat Account </a>
-</div>
-<div class="col s12 m2">
-<div class="col s6  "><a class="waves-effect waves-light btn" >  Login  </a>
+<div class="col s12 "><a class="waves-effect waves-light btn" >  Login  </a>
 <p class="z-depth 2"></p>
 
 </div>
 </div>
 </div>
 
-
+</div>
 
 <?php include 'include/template/footer.php' ?>
