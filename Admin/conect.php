e8b4856fa81a979f447159132092ee395424ddfb
SoftwareGroup2019 <mohamadit69@gmail.com>
2019-11-12 21:05:41 +0300

    connection done

--- Admin/conect.php
+++ Admin/conect.php
@@ -7,11 +7,7 @@ $pass= "";
 try {
   $con = new PDO ($dsn ,$user, $pass);
   $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-<<<<<<< HEAD
   echo "conection done";
-=======
-  echo 'Cnnection success ';
->>>>>>> f3f6107cb26b3900c6bda2d043a906e37bcb9526
 
 }
 catch(PDOException $e){
