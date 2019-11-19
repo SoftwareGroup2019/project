234d4cae58abbc14e8e91961078ea35d92d27f4a
SoftwareGroup2019 <mohamadit69@gmail.com>
2019-11-20 01:48:15 +0300

    logout dubara tawaw bu

--- Admin/include/template/navbar.php
+++ Admin/include/template/navbar.php
@@ -3,7 +3,7 @@
   <li><a href="#!">Edit</a></li>
   <li><a href="#!">Settings</a></li>
   <li class="divider"></li>
-  <li><a href="#!">Logout</a></li>
+  <li><a href="logout.php">Logout</a></li>
 </ul>
 <nav>
   <div class="nav-wrapper">
@@ -14,7 +14,7 @@
         <li><a href="Members.html">Members</a></li>
       <li><a href="Categories.html">Categories</a></li>
       <!-- Dropdown Trigger -->
-      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li><a class="dropdown-trigger" href="logout.php" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
   </div>
 </nav>
