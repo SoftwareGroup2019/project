9cdf2bc699b6bae99d464b6b2e32d1dc7231f28b
softwaregroup2019 <mohamadit69@gmail.com>
2019-11-26 23:38:15 +0300

    part 2 tawaw bu members page @yosef raais

--- Admin/members.php
+++ Admin/members.php
@@ -14,61 +14,86 @@ $do = isset($_GET['do'])? $_GET['do']: 'manage';
 if ($do =='manage')
 {
 
+// mange page
+
 echo "this is manage page";
 
 }
-elseif($do=='Edit'){
-  ?>
-<div class="containe">
+elseif($do=='Edit'){ //Edit page
+  //chek if get request userid is numeric &get the int value of it
+  $userid = isset ($_GET['userid'])&& is_numeric($_GET['userid']) ? intval ($_GET['userid']) : 0;
+ //select all data depend on this ID
+  }
+    $stmt = $con->prepare("SELECT * FROM user WHERE UserID = ? Limit  1");
+
+//execute query
+
+    $stmt->execute(array($userid));
+
+//fetch the data
+
+    $row=$stmt->fetch();
+
+//the row count
+
+    $count =$stmt->rowCount();
+
+//if ther is such id show the form
+
+    if ($stmt->rowCount() > 0 {  ?>
+
+  <div class="containe">
 
 
-  <div class="row">
- <form class="col s9">
-   <div class="row">
-     <div class="input-field col s6">
-       <i class="material-icons prefix">account_circle</i>
-       <input id="icon_prefix" type="text" class="validate">
-       <label for="icon_prefix">UserName</label>
-     </div>
-   </div>
-</form>
- </div>
 
-   <div class="row">
-  <form class="col s9">
     <div class="row">
-      <div class="input-field col s9">
-        <i class="material-icons prefix">account_circle</i>
-        <input id="icon_prefix" type="text" class="validate">
-        <label for="icon_prefix">FullName</label>
-      </div>
-    </div>
-</form>
-  </div>
+   <form class="col s9">
+     <div class="row">
+       <div class="input-field col s6">
+         <i class="material-icons prefix">account_circle</i>
+         <input id="icon_prefix" type="text" class="validate" value="<?php echo " $row ['UserName'] >
+         <label for="icon_prefix">UserName</label>
 
+       </div>
+     </div>
+  </form>
+   </div>
+
+     <div class="row">
+    <form class="col s9">
+      <div class="row">
+        <div class="input-field col s9">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate"> value="<?php echo " $row ['FullName'] >
+          <label for="icon_prefix">FullName</label>
+        </div>
+      </div>
+  </form>
+    </div>
+
+
+          <div class="row">
+         <form class="col s9">
+           <div class="row">
+             <div class="input-field col s9">
+               <i class="material-icons prefix">lock</i>
+            <input id="password" type="password" class="validate"  value="<?php echo " $row ['Password'] >
+            <label for="password">Password</label>
+          </div>
+        </div>
 
         <div class="row">
        <form class="col s9">
          <div class="row">
            <div class="input-field col s9">
-             <i class="material-icons prefix">lock</i>
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-
-      <div class="row">
-     <form class="col s9">
-       <div class="row">
-         <div class="input-field col s9">
-           <i class="material-icons prefix">email</i>
-              <input id="email" type="email" class="validate">
-              <label for="email">Email</label>
-              <span class="helper-text" data-error="wrong Email" data-success="right Email">Helper text</span>
+             <i class="material-icons prefix">email</i>
+                <input id="email" type="email" class="validate" value="<?php echo " $row ['Email'] >
+                <label for="email">Email</label>
+                <span class="helper-text" data-error="wrong Email" data-success="right Email">Helper text</span>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
 
             <div class="row">
            <form class="col s9">
@@ -76,8 +101,15 @@ elseif($do=='Edit'){
         <i class="material-icons right"></i>
       </button>
   <?php
+
+
+
+
 }
 
+else {
+  echo   'theres no such ID';
+}
 
 
 
