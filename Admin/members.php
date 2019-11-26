3121ad9b03e43c3701ea3c023e43b846589bf38e
softwaregroup2019 <mohamadit69@gmail.com>
2019-11-25 20:09:33 -0800

    Edit tawaw #Hawar

--- Admin/members.php
+++ Admin/members.php
@@ -18,8 +18,64 @@ echo "this is manage page";
 
 }
 elseif($do=='Edit'){
-  echo 'welcome To Edit page';
+  ?>
+<div class="containe">
 
+
+  <div class="row">
+ <form class="col s9">
+   <div class="row">
+     <div class="input-field col s6">
+       <i class="material-icons prefix">account_circle</i>
+       <input id="icon_prefix" type="text" class="validate">
+       <label for="icon_prefix">UserName</label>
+     </div>
+   </div>
+</form>
+ </div>
+
+   <div class="row">
+  <form class="col s9">
+    <div class="row">
+      <div class="input-field col s9">
+        <i class="material-icons prefix">account_circle</i>
+        <input id="icon_prefix" type="text" class="validate">
+        <label for="icon_prefix">FullName</label>
+      </div>
+    </div>
+</form>
+  </div>
+
+
+        <div class="row">
+       <form class="col s9">
+         <div class="row">
+           <div class="input-field col s9">
+             <i class="material-icons prefix">lock</i>
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+
+      <div class="row">
+     <form class="col s9">
+       <div class="row">
+         <div class="input-field col s9">
+           <i class="material-icons prefix">email</i>
+              <input id="email" type="email" class="validate">
+              <label for="email">Email</label>
+              <span class="helper-text" data-error="wrong Email" data-success="right Email">Helper text</span>
+            </div>
+          </div>
+        </form>
+      </div>
+
+            <div class="row">
+           <form class="col s9">
+      <button class="btn waves-effect waves-light" type="submit" name="action">Save
+        <i class="material-icons right"></i>
+      </button>
+  <?php
 }
 
 
