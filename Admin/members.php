b6ef94a671d20a78fe3f58fb6593fb9745c8d0b7
SoftwareGroup2019 <mohamadit69@gmail.com>
2019-11-30 20:15:17 +0300

    big big bug fixed

--- Admin/members.php
+++ Admin/members.php
@@ -68,7 +68,7 @@ $stmt = $con->prepare("SELECT * FROM user WHERE UserID = ? LIMIT 1");
         <!-- Username -->
        <div class="input-field col s12">
          <i class="material-icons prefix">account_circle</i>
-         <input id="icon_prefix" type="text" name="user" class="validate" value="<?php echo $row ['Username'];?>>
+         <input id="icon_prefix" type="text" name="user" class="validate" value="<?php echo $row ['Username'];?>">
          <label for="icon_prefix">UserName</label>
        </div>
        <!-- ////////////////// -->
@@ -173,144 +173,136 @@ else
   else{
        echo "Sorry You Cant Brouse This Page Directly";
     }
-    echo "</div>";
-    }
-  }
-  else{
-       echo "Sorry You Cant Brouse This Page Directly";
-    }
-    echo "</div>";
-  }
+
+  } //end of post update requst
+  } // end of update
+
+elseif ($do =='add') {
+      // code..
+      ?>
+      <div class="container">
+
+  <h4 class="center">Add Users</h4>
+
+       <!-- start of form -->
+       <form class="col s12" action="?do=insert" method="POST">
 
 
-  elseif ($do =='add') {
-    // code..
-    ?>
-    <div class="container">
+         <!-- start of row -->
+         <div class="row">
 
-<h4 class="center">Add Users</h4>
+            <!-- Username -->
+           <div class="input-field col s12">
+             <i class="material-icons prefix">account_circle</i>
+             <input id="icon_prefix" type="text" name="user" class="validate" required="required">
+             <label for="icon_prefix">UserName</label>
+           </div>
+           <!-- ////////////////// -->
 
-     <!-- start of form -->
-     <form class="col s12" action="?do=Update" method="POST">
-
-
-       <!-- start of row -->
-       <div class="row">
-
-          <!-- Username -->
-         <div class="input-field col s12">
-           <i class="material-icons prefix">account_circle</i>
-           <input id="icon_prefix" type="text" name="user" class="validate" required="required">
-           <label for="icon_prefix">UserName</label>
-         </div>
-         <!-- ////////////////// -->
-
-          <!-- Full Name -->
-         <div class="input-field col s12">
-           <i class="material-icons prefix">account_circle</i>
-           <input id="icon_prefix" type="text" name="full" required="required">
-           <label for="icon_prefix">FullName</label>
-         </div>
-         <!-- ////////////////// -->
+            <!-- Full Name -->
+           <div class="input-field col s12">
+             <i class="material-icons prefix">account_circle</i>
+             <input id="icon_prefix" type="text" name="full" required="required">
+             <label for="icon_prefix">FullName</label>
+           </div>
+           <!-- ////////////////// -->
 
 
-        <!-- Email -->
-         <div class="input-field col s12">
-           <i class="material-icons prefix">email</i>
-           <input id="icon_prefix" type="email" name="email" class="validate" required="required">
-           <label for="icon_prefix">Email</label>
-         </div>
-         <!-- ////////////////// -->
+          <!-- Email -->
+           <div class="input-field col s12">
+             <i class="material-icons prefix">email</i>
+             <input id="icon_prefix" type="email" name="email" class="validate" required="required">
+             <label for="icon_prefix">Email</label>
+           </div>
+           <!-- ////////////////// -->
 
-         <!-- Password -->
-         <div class="input-field col s12">
-           <i class="material-icons prefix">lock</i>
-        <input id="password" type="password" name="newpassword" class="validate">
-        <label for="password">Password</label>
-        </div>
-      <!-- ////////////////// -->
-
-        <!-- Buttton -->
-      <button class="btn waves-effect waves-light" type="submit" name="action">Add
-        <i class="material-icons right"></i>
-      </button>
+           <!-- Password -->
+           <div class="input-field col s12">
+             <i class="material-icons prefix">lock</i>
+          <input id="password" type="password" name="newpassword" class="validate">
+          <label for="password">Password</label>
+          </div>
         <!-- ////////////////// -->
 
-    </div>
-    <!-- end of row -->
+          <!-- Buttton -->
+        <button class="btn waves-effect waves-light" type="submit" name="action">Add
+          <i class="material-icons right"></i>
+        </button>
+          <!-- ////////////////// -->
 
-    </form>
-    <!-- end of form  -->
-     </div>
-     <!-- end of container -->
+      </div>
+      <!-- end of row -->
+
+      </form>
+      <!-- end of form  -->
+       </div>
+       <!-- end of container -->
 
 
 
 
-  <?php
+    <?php
 
 
-  }
+  } // end of add
 
   elseif ($do == 'insert') {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-      echo   " <h1 class = 'text-center'> Update Member </h>";
-      echo "<div class='container'>";
+      if ($_SERVER['REQUEST_METHOD'] == 'POST')
+      {
+        echo   " <h1 class = 'center'> Add Users </h1>";
 
-      $user = $_POST['user'];
-        $pass = $_POST['password'];
-      $email = $_POST['email'];
-      $name = $_POST['full'];
-$hashpass=sha1($_POST['email']);
 
-      // agar inputakan ba bataly baje bely away xware tatbyq dabi
-      $formErrors = array();
-      if (strlen($user)<4){
-        $formErrors[] = 'UserName cant Be Less Than <strong> 4 Characters</strong>';
+        $user = $_POST['user'];
+        $pass = $_POST['newpassword'];
+        $email = $_POST['email'];
+        $name = $_POST['full'];
+
+
+        // agar inputakan ba bataly baje bely away xware tatbyq dabi
+        $formErrors = array();
+        // agar username zyatr by la20 error dada away xware lo awaya
+        if (strlen($user)>20){
+          $formErrors[] = 'UserN cant Be more Than <strong> 20 Characters</strong>';
+        }
+      if(empty($pass)){
+            $formErrors[] = 'password cant Be <strong> Empty </strong>';
+        }
+        if(empty($name)){
+          $formErrors[] = 'Full Name cant Be <strong> Empty </strong>';
+        }
+        if(empty($email)){
+          $formErrors[] = 'Email cant Be <strong> Empty </strong>';
+        }
+        // loop labo away bzany error haya
+        foreach($formErrors as $error){
+          echo ' <div class="alert alert-danger" ' .  $error .'</div>';
+
+        }
+        if (empty($formErrors)){
+
+        //  echo $id . $user . $email . $name;
+        // update zanyryakany usery la naw database dakay
+
+       echo "Done";
+       
+
+     } // end of empty error
+
+
+
+    } // end of post insert requst
+
+    else{
+         echo "Sorry You Cant Brouse This Page Directly";
       }
-      // agar username zyatr by la20 error dada away xware lo awaya
-      if (strlen($user)>20){
-        $formErrors[] = 'UserN cant Be more Than <strong> 20 Characters</strong>';
-      }
-      // awanash har awhaya agar batal bi aw ifana tatbyq dabi barxola
-      if(empty($user)){
-        $formErrors[] = 'UserName cant Be <strong> Empty </strong>';
-
-        if(empty($pass)){
-          $formErrors[] = 'password cant Be <strong> Empty </strong>';
-      }
-      if(empty($name)){
-        $formErrors[] = 'Full Name cant Be <strong> Empty </strong>';
-      }
-      if(empty($email)){
-        $formErrors[] = 'Email cant Be <strong> Empty </strong>';
-      }
-      // loop labo away bzany error haya
-      foreach($formErrors as $error){
-        echo ' <div class="alert alert-danger" ' .  $error .'</div>';
-
-      }
-      if (empty($formErrors)){
-
-      //  echo $id . $user . $email . $name;
-      // update zanyryakany usery la naw database dakay
-
-      echo "<div class='alert alert-success'>" . $stmt->rowCount() . ' Record Inserted</div>';
-    }
-  }
-  else{
-       echo "Sorry You Cant Brouse This Page Directly";
-    }
-    echo "</div>";
-  }
+  } // end of insert
 
 
 
 
-include 'include/template/footer.php';
-}
-// Reuest end here
+    include 'include/template/footer.php';
+  } /// end of session
+
 else
 {
   header('location: index.php');
