8e094340953a8f6a5afffbe5ebdc35bf0e8d4ea0
SoftwareGroup2019 <mohamadit69@gmail.com>
2020-04-10 17:51:10 +0300

    User Interface/VIP 15 task pekawa tawaw #Ayman Chaw Zard

---
+++ company website/js/ttt.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+ $('.collapsible').collapsible();
+
+    $('.modal').modal();
+    $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+  });
+
+  // our work section
+  $("#btn-all").click(function() {
+    $(".item .item").slideDown();
+  });
+  $("#btn-web").click(function() {
+    $(".item .item").fadeout();
+    $(".item .web").fadein();
+  });
+
+  $("#btn-mobaile").click(function() {
+    $(".item .item").hide("slow");
+    $(".item .mobaile").show("slow");
+  });
+
+  $("#btn-desk").click(function() {
+    $(".item .item").slideUp();
+    $(".item .desk").slideDown();
+  });
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+
+  });
+   $('.parallax').parallax();
+   $('a').click(function(){
+     $('html, body').animate({
+       scrollTop: $( $(this).attr('herf') ).offset().top
+     },800);
+   });
+   // Loadding Screen
+   $(window).load(function(){
+     $(".loadding-screen .preloader-wrapper").fadeout(2000,
+     function(){
+       $(this).parent().function(1500)
+     });
+   })
