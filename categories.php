05c7777e7f89c3e6979ce65c83d8e12905e39759
softwaregroup2019 <mohamadit69@gmail.com>
2020-05-10 12:36:43 -0700

    Create item Box | User Side #Hawar

--- categories.php
+++ categories.php
@@ -9,21 +9,35 @@
 echo str_replace('-','',$_GET['pagename'])
 ?>
 </h1>
+<br>
 <div class="row">
+
+
+
+
+
+
+
 <?php
- foreach (getitems($_GET['pageid']) as $item) {
-  echo '<div class="col-sm-6 col-md-4">';
-echo '<div class="thumbnail item-bom">';
-/*lera rasmakam danaya*/
-echo '<img class="" src="haha.png" alt="dddd" />';
-echo '<div class="caption">';
-echo'<h3>'.$item['Name'].'</h3>';
-echo'<p>'.$item['Description'].'</p>';
+ foreach (getitems($_GET['pageid']) as $item)
+{
+
+echo '<div class="col-sm-12 col-md-4 col-lg-3">';
+echo '<div class="card">';
+echo '<img src="layout/img/haha.png" alt="Denim Jeans" style="width:100%">';
+echo '<h1>'.$item['Name'].'</h1>';
+echo '<p class="price">'.$item['Price'].'</p>';
+echo '<a href="#" class="btn btn-primary" type="button">
+';
+echo 'Read More...';
+echo '</a>';
 echo '</div>';
 echo '</div>';
-echo '</div>';
+
 }
 ?>
+</div>
+
 </div>
 
 <?php include 'include/template/footer.php' ?>
