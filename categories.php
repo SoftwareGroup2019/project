5a47f86dcf04f98ada78f525d2578665f1a0df3a
softwaregroup2019 <mohamadit69@gmail.com>
2020-05-09 17:17:29 -0700

    kamak la Create item Box | User Side #Hawar

--- categories.php
+++ categories.php
@@ -9,9 +9,19 @@
 echo str_replace('-','',$_GET['pagename'])
 ?>
 </h1>
+<div class="row">
 <?php
  foreach (getitems($_GET['pageid']) as $item) {
-  echo $item ['Name'];
+  echo '<div class="col-sm-6 col-md-4">';
+echo '<div class="thumbnail item-bom">';
+/*lera rasmakam danaya*/
+echo '<img class="" src="haha.png" alt="dddd" />';
+echo '<div class="caption">';
+echo'<h3>'.$item['Name'].'</h3>';
+echo'<p>'.$item['Description'].'</p>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
 }
 ?>
 </div>
