271cfc23f800d34749169c2cc9d5305b111862a1
softwaregroup2019 <mohamadit69@gmail.com>
2020-05-14 12:09:34 -0700

    TAMAM #HAWAR

--- include/template/navbar.php
+++ include/template/navbar.php
@@ -1,6 +1,12 @@
 <div class="upper-bar">
-Upper Bar
+  <div class="container">
+    <a href="logout.php"> <span class="pull-right">Login/signup</span>
+      </a>
+    </div>
+
 </div>
+<br>
+
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #74b9ff;">
   <a class="navbar-brand" href="#">ShipShop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- layout/js/front.js
+++ layout/js/front.js
@@ -11,18 +11,43 @@ $('.sidenav').sidenav();
 
 
   //   xatw hawar lera kodakant bnwsa
-
-
-
-
-
+  </form>
+  <form class="signup">
+  <input
+   class="form-control"
+   type="text"
+  name="username"
+  autocmplete="off"
+  placeeholder="Type your user name"
+  required="required"
+  />
+  <input
+   class="form-control"
+   type="password"
+    name="password"
+     autocmplete="new-password"
+     placeeholder="Type Complex password" />
+     <input
+      class="form-control"
+      type="password"
+       name="password2"
+        autocmplete="new-password"
+        placeeholder="Type a password again" />
+     <input
+      class="form-control"
+      type="emali"
+       name="emali"
+        placeeholder="Type a valid email" />
+  <input
+  class="btn btn-success btn-block"
+  type="submit"
+  value="signup" />
+  </form>
+  </div>
 
   /////////////////
 
   });
-
-
-
 
 
 
@@ -42,4 +67,6 @@ $('.sidenav').sidenav();
          timeDelete: 50,
 
          // animation delay
-         timePause: 1000});
+         timePause: 1000
+
+       });
